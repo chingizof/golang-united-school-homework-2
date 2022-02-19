d75cd606c11710f2726aab7cb91052c6a6006219
Mister-718 <59221853+Mister-718@users.noreply.github.com>
2022-02-20 02:05:23 +0600

    solution

--- solution.go
+++ solution.go
@@ -11,12 +11,12 @@ import (
 // CalcSquare(10.0, SidesTriangle)
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
+var Pi = 3.14159265358979323846264338327950288419716939937510582097494459
 
 func CalcSquare(sideLen float64, sidesNum int) float64 {
-	pi := math.Pi
 
 	if sidesNum == 0 {
-		return sideLen * pi * pi
+		return sideLen * Pi * Pi
 	} else if sidesNum == 4 {
 		return sideLen * sideLen
 	} else if sidesNum == 3 {
